39c627025a36d78cc13a42f145b2788d3832088b
John Hoestje <john.hoestje+GITHUB@gmail.com>
2025-03-07 17:45:39 -0600

    Remove old example package directory

--- SpringBatchPgaiApp/src/main/java/com/johoco/springbatchpgaiapp/PgAiDocumentProcessorApplication.java
+++ SpringBatchPgaiApp/src/main/java/com/johoco/springbatchpgaiapp/PgAiDocumentProcessorApplication.java
@@ -6,8 +6,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class PgAiDocumentProcessorApplication {
+public class SpringBatchPgaiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PgAiDocumentProcessorApplication.class, args);
+        SpringApplication.run(SpringBatchPgaiApplication.class, args);
     }
 }
