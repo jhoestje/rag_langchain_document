fd6067a6c20fbecbe3e7aa3c94e212eeb9841ca7
John Hoestje <john.hoestje+GITHUB@gmail.com>
2025-03-07 19:09:57 -0600

    Improve Document model: - Add unique constraint on filename to prevent duplicates - Make filename required with @Column(nullable = false) - Change content to text type for larger documents - Remove unused contentType field

--- SpringBatchPgaiApp/src/main/java/com/johoco/springbatchpgaiapp/model/Document.java
+++ SpringBatchPgaiApp/src/main/java/com/johoco/springbatchpgaiapp/model/Document.java
@@ -6,19 +6,23 @@ import org.hibernate.annotations.Type;
 
 @Data
 @Entity
-@Table(name = "documents")
+@Table(name = "documents", uniqueConstraints = {
+    @UniqueConstraint(columnNames = "filename")
+})
 public class Document {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String filename;
+    
+    @Column(columnDefinition = "text")
     private String content;
 
     @Column(name = "embedding", columnDefinition = "vector")
     private float[] embedding;
 
-    private String contentType;
     private Long fileSize;
     private String status;
     
